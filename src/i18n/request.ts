20b93917cf1b27a9c93fc5d2b19825d2709a7f4a
phanuel tsopze <146930921+alibia-phanuel@users.noreply.github.com>
2025-02-01 10:31:49 +0100

    hero section

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -4,6 +4,7 @@ export default getRequestConfig(async ({ requestLocale }) => {
   // This typically corresponds to the `[locale]` segment
   let locale = await requestLocale;
   // Ensure that a valid locale is used
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   if (!locale || !routing.locales.includes(locale as any)) {
     locale = routing.defaultLocale;
   }
